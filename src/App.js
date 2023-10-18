cde21315dda4aac50336d9457b38ecc4f1269b09
Jon Ortiz <jonnatano94@gmail.com>
2023-10-18 10:30:47 -0700

    First commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,64 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
+import { useEffect, useState } from 'react';
+import SearchIcon from './search.svg';
+import MovieCard from './MovieCard';
 
-function App() {
+const API_KEY = 'c6f99c51'
+const API_URL = `http://www.omdbapi.com?apikey=${API_KEY}`;
+
+const  App = () => {
+
+  const [searchTerm, setSearchTerm] = useState("")
+  const [movies, setMovies] = useState([]);
+
+  const searchMovie = async (title) => {
+    const response = await fetch(`${API_URL}&s=${title}`)
+    const data = await response.json();
+    setMovies(data.Search)
+  }
+
+  const onHitEnter = (e) => {
+    var code = e.key;
+    // console.log("code: ", code)
+    if (code === 'Enter') {
+      searchMovie(searchTerm)
+    }
+  }
+
+  useEffect(() => {
+    searchMovie('Spiderman')
+  }, [])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+     <h1>MovieLand</h1>
+
+     <div className='search'>
+        <input
+          placeholder='Search for movies'
+          value={searchTerm}
+          onChange={(e) => setSearchTerm(e.target.value)}
+          onKeyPress={onHitEnter}/>
+        <img 
+          src={SearchIcon}
+          alt="search"
+          onClick={() => searchMovie(searchTerm)}
+          />
+     </div>
+
+     {movies?.length > 0
+       ? (
+          <div className='container'>
+            {movies.map((movie, idx) => (
+              <MovieCard key={idx} movie={movie}/>
+            ))}
+          </div> 
+        ) : (
+          <div className='empty'>
+            <h1>No movies found! </h1>
+          </div>
+        )}
     </div>
   );
 }
